757c8abc9346326bf6e75a037631f5c06e1f4707
dobri-dobrev <dmd2169@columbia.edu>
2014-03-09 22:07:15 -0400

    nice plot

--- public/js/index.js
+++ public/js/index.js
@@ -1,21 +1,47 @@
 $(function(){
-	var data = [];
-	data.push([0, 90]);
-	data.push([1, 95]);
-	data.push([2, 80]);
-	data.push([3, 90]);
-	data.push([4, 95]);
-	data.push([5, 80]);
-	data.push([6, 90]);
-	data.push([7, 95]);
-	data.push([8, 80]);
-	data.push([9, 90]);
-	data.push([10, 95]);
-	data.push([11, 80]);
-	data = [data];
-	$.plot($("#placeholder"), data, { yaxis: { max: 100 },
+	var ser1 = [];
+	ser1.push([0, 90]);
+	ser1.push([1, 91]);
+	ser1.push([2, 88]);
+	ser1.push([3, 87]);
+	ser1.push([4, 93]);
+	ser1.push([5, 95]);
+	ser1.push([6, 91]);
+	ser1.push([7, 92]);
+	ser1.push([8, 93]);
+	ser1.push([9, 94]);
+	ser1.push([10, 94]);
+	ser1.push([11, 94]);
+	var ser2 = [];
+	ser2.push([0, 88]);
+	ser2.push([1, 92]);
+	ser2.push([2, 86]);
+	ser2.push([3, 88]);
+	ser2.push([4, 91]);
+	ser2.push([5, 97]);
+	ser2.push([6, 93]);
+	ser2.push([7, 94]);
+	ser2.push([8, 89]);
+	ser2.push([9, 91]);
+	ser2.push([10, 92]);
+	ser2.push([11, 92]);
+
+
+
+
+
+	data = [ser1,ser2];
+	$.plot($("#placeholder"), data, { 
+		yaxis: { 
+			min: 85, 
+			max: 100,
+			axisLabel: 'Retention'
+		},
 		xaxis: {
-		    ticks: [[0,'Jan'],[1,'Feb'],[2,'Mar'],[3,'April'],[4,'May'],[5,'June'],[6,'July'],[7,'Aug'],[8,'Sep'],[9,'Oct'],[10,'Nov'],[11,'Dec']]
-		  }
+		    ticks: [[0,'Jan'],[1,'Feb'],[2,'Mar'],[3,'Apr'],[4,'May'],[5,'Jun'],[6,'Jul'],[7,'Aug'],[8,'Sep'],[9,'Oct'],[10,'Nov'],[11,'Dec']],
+		    show: true,
+		    axisLabel: 'Time',
+		    position: 'bottom'
+		  },
 	 });
 });
