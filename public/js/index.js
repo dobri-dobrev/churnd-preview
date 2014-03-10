d317f19b48c69fe588b17622ad19b9ebd32f0805
dobri-dobrev <dmd2169@columbia.edu>
2014-03-09 21:40:29 -0400

    string based axese

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,12 @@
 $(function(){
 	var data = [];
-	data.push(['J', 90]);
-	data.push(['F', 95]);
-	data.push(['M', 80]);
-	$.plot($("#placeholder"), data, { yaxis: { max: 100 } });
+	data.push([0, 90]);
+	data.push([1, 95]);
+	data.push([2, 80]);
+	data = [data];
+	$.plot($("#placeholder"), data, { yaxis: { max: 100 },
+		xaxis: {
+		    ticks: [[0,'Bob'],[1,'Chris'],[2,'Joe']]
+		  }
+	 });
 });
