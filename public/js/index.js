0a23ce11ac9fdeb3c5e7a8411d5f746d480c38ef
dobri-dobrev <dmd2169@columbia.edu>
2014-03-09 22:29:42 -0400

    zooom

--- public/js/index.js
+++ public/js/index.js
@@ -26,22 +26,44 @@ $(function(){
 	ser2.push([10, 92]);
 	ser2.push([11, 92]);
 
+	var dataPie = [
+		{ label: "No Risk",  data: 45},
+		{ label: "Low Risk",  data: 38},
+		{ label: "Moderate Risk",  data: 12},
+		{ label: "High Risk",  data: 5}
+	];
 
-
-
+	$.plot('#retention_chart', dataPie, {
+	    series: {
+	        pie: {
+	            show: true
+	        }
+	    },
+	    legend: {
+	        show: false
+	    }
+	});
 
 	data = [ser1,ser2];
 	$.plot($("#placeholder"), data, { 
 		yaxis: { 
 			min: 85, 
 			max: 100,
-			axisLabel: 'Retention'
+			axisLabel: 'Retention',
+			zoomRange: [1, 10]
 		},
 		xaxis: {
 		    ticks: [[0,'Jan'],[1,'Feb'],[2,'Mar'],[3,'Apr'],[4,'May'],[5,'Jun'],[6,'Jul'],[7,'Aug'],[8,'Sep'],[9,'Oct'],[10,'Nov'],[11,'Dec']],
 		    show: true,
 		    axisLabel: 'Time',
-		    position: 'bottom'
+		    position: 'bottom',
+		    zoomRange: [1, 10]
 		  },
+		zoom: {
+	            interactive: true
+        },
+        pan: {
+            interactive: true
+        }
 	 });
 });
