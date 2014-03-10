3721bd2f9f1ff6ad1bfdb024c4d0135acb8701a2
dobri-dobrev <dmd2169@columbia.edu>
2014-03-09 21:47:00 -0400

    months

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,19 @@ $(function(){
 	data.push([0, 90]);
 	data.push([1, 95]);
 	data.push([2, 80]);
+	data.push([3, 90]);
+	data.push([4, 95]);
+	data.push([5, 80]);
+	data.push([6, 90]);
+	data.push([7, 95]);
+	data.push([8, 80]);
+	data.push([9, 90]);
+	data.push([10, 95]);
+	data.push([11, 80]);
 	data = [data];
 	$.plot($("#placeholder"), data, { yaxis: { max: 100 },
 		xaxis: {
-		    ticks: [[0,'Bob'],[1,'Chris'],[2,'Joe']]
+		    ticks: [[0,'Jan'],[1,'Feb'],[2,'Mar'],[3,'April'],[4,'May'],[5,'June'],[6,'July'],[7,'Aug'],[8,'Sep'],[9,'Oct'],[10,'Nov'],[11,'Dec']]
 		  }
 	 });
 });
