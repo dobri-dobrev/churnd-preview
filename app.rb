c960a6be0632fa260fd6ba163473888ef3cd42b1
churnd <churndcommon@gmail.com>
2014-03-13 21:44:58 -0400

    submission and loggin

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require 'sinatra'
+require 'json'
+require 'pony'
+
+configure :production, :development do
+    enable :logging
+end
+before {
+  env['rack.logger'] = Logger.new "#{settings.root}/log/#{settings.environment}.log"
+}
 
 get '/' do
 	erb :index
@@ -11,3 +20,11 @@ end
 get '/campaign' do
 	erb :campaign
 end
+
+post '/send_email' do
+	puts params[:email]
+	logger.info params[:email]
+	content_type :json
+	{ :key1 => 'value1', :key2 => 'value2' }.to_json
+end
+
